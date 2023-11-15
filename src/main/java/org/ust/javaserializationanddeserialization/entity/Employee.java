f539cfdae4adb66a3ed2bf87e6c7c4f2328a16cc
Mark Bryan Calupig <markbryancalupig@gmail,com>
2023-11-15 14:45:31 +0800

    Serialization and Deserialization Based codes

---
+++ src/main/java/org/ust/javaserializationanddeserialization/entity/Employee.java
@@ -0,0 +1,15 @@
+package org.ust.javaserializationanddeserialization.entity;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    public String name;
+    public String address;
+
+
+
+
+
+
+
+}
