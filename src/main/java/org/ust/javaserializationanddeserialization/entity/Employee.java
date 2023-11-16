382f13faab91f582d1a1ff2a6a218f795f21b539
jbtbeltranUST <JamesBryan.Beltran@ust.com>
2023-11-16 21:31:38 +0800

    update on files

--- src/main/java/org/ust/javaserializationanddeserialization/entity/Employee.java
+++ src/main/java/org/ust/javaserializationanddeserialization/entity/Employee.java
@@ -6,10 +6,19 @@ public class Employee implements Serializable {
     public String name;
     public String address;
 
+    public String role;
+    public Employee (String name, String address, String role) {
+        this.name = name;
+        this.address = address;
+        this.role = role;
+    }
 
-
-
-
-
-
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
